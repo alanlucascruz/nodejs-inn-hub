796cdab96a2fc77c52cb8bf4023c89b57f88e149
Alan Cruz <alanlucascruz@gmail.com>
2025-10-31 00:23:44 -0300

    Mongoose; User Schema

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import express, { Express } from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 import routes from "./routes";
 
 const app: Express = express();
 const port: Number = 3001;
+
+const connectDatabase = async () => {
+  const user: string = <string>process.env.DB_USER;
+  const password: string = <string>process.env.DB_PASSWORD;
+  const uri: string = `mongodb+srv://${user}:${password}@cluster0.isbttru.mongodb.net/?appName=Cluster0`;
+
+  mongoose.set("strictQuery", true);
+
+  await mongoose.connect(uri);
+};
 
 const applyMiddlewares = () => {
   app.use(cors());
@@ -17,7 +28,8 @@ const startServer = () => {
   });
 };
 
-(() => {
+(async () => {
+  await connectDatabase();
   applyMiddlewares();
   startServer();
 })();
